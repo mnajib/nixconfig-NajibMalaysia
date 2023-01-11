a028ea67b53f1f1fa2a1d0844e4e177561c63022
Najib Ibrahim <mnajib@gmail.com>
2023-01-11 21:14:41 +0800

    color scheme xresources

--- home-manager/common.nix
+++ home-manager/common.nix
@@ -394,14 +394,14 @@ let
     };
     #xresources.extraConfig = builtins.readFile ./src/.Xresources;
     #xresources.extraConfig = builtins.readFile ( pkgs.fetchFromGitHub {} + "./src/.Xresources");
-    #xresources.extraConfig = builtins.readFile (
-    #  pkgs.fetchFromGitHub {
-    #    owner = "solarized";
-    #    repo = "xresources";
-    #    rev = "025ceddbddf55f2eb4ab40b05889148aab9699fc";
-    #    sha256 = "0lxv37gmh38y9d3l8nbnsm1mskcv10g3i83j0kac0a2qmypv1k9f";
-    #  } + "/Xresources.dark"
-    #);
+    xresources.extraConfig = builtins.readFile (
+      pkgs.fetchFromGitHub {
+        owner = "solarized";
+        repo = "xresources";
+        rev = "025ceddbddf55f2eb4ab40b05889148aab9699fc";
+        sha256 = "0lxv37gmh38y9d3l8nbnsm1mskcv10g3i83j0kac0a2qmypv1k9f";
+      } + "/Xresources.dark"
+    );
 
   gtk = {
     enable = true;
