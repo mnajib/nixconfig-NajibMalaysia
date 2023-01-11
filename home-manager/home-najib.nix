3414341179dc9619590e0f6c50892c29890b1fc4
Najib Ibrahim <mnajib@gmail.com>
2023-01-11 23:29:40 +0800

    add some packages for hame-manager najib

--- home-manager/home-najib.nix
+++ home-manager/home-najib.nix
@@ -50,7 +50,25 @@
 
   # Add stuff for your user as you see fit:
   # programs.neovim.enable = true;
-  # home.packages = with pkgs; [ steam ];
+  home.packages = with pkgs; [
+    #steam
+    almanah
+    #file-roller
+    heimer
+    jrnl
+    kodi
+    #python3.8-notebook
+    qtox
+    retroarch
+    tig
+    treesheets
+    unrar
+    vue
+    vym
+    xarchiver
+    #xmind-8-update8
+    xournalpp
+  ];
 
   # Enable home-manager and git
   programs.home-manager.enable = true;
