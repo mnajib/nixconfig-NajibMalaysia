0cf1f0388ad73e44cf078c0ed12a9563e8b0c572
Najib Ibrahim <mnajib@gmail.com>
2023-01-11 20:34:28 +0800

    fix indent in host-customdesktop.nix

--- nixos/host-customdesktop.nix
+++ nixos/host-customdesktop.nix
@@ -2,164 +2,164 @@
 
 { pkgs, config, ... }:
 {
-    nix = {
-        package = pkgs.nixFlakes;
-		extraOptions = ''
-			experimental-features = nix-command flakes
-		'';
-    };
+  nix = {
+    package = pkgs.nixFlakes;
+    extraOptions = ''
+      experimental-features = nix-command flakes
+    '';
+  };
 
-    imports = [
-        #./hardware-configuration-tv.nix
-		# rename to
-        ./hardware-configuration-customdesktop.nix
+  imports = [
+    #./hardware-configuration-tv.nix
+    # rename to
+    ./hardware-configuration-customdesktop.nix
 
-        #./bootEFI.nix
-        #./bootBIOS.nix
-        
-        #./thinkpad.nix
+    #./bootEFI.nix
+    #./bootBIOS.nix
 
-		# Disable this; as we can just set custom DNS in NetworkManager
-        #./network-dns.nix
+    #./thinkpad.nix
 
-        ./users-anak2.nix
+    # Disable this; as we can just set custom DNS in NetworkManager
+    #./network-dns.nix
 
-        #./anbox.nix
-        ./virtualbox.nix
+    ./users-anak2.nix
 
-		./typesetting.nix
+    #./anbox.nix
+    ./virtualbox.nix
 
-        #./syncthing.nix
-        #./nextcloud.nix
+    ./typesetting.nix
 
-		./nfs-client.nix
+    #./syncthing.nix
+    #./nextcloud.nix
 
-		./console-keyboard-dvorak.nix
-		./keyboard-with-msa.nix
+    ./nfs-client.nix
 
-		#./audio-pulseaudio.nix
-		./audio-pipewire.nix
+    ./console-keyboard-dvorak.nix
+    ./keyboard-with-msa.nix
 
-		#./synergy-client.nix # barrier
+    #./audio-pulseaudio.nix
+    ./audio-pipewire.nix
 
-		./hardware-printer.nix
-		./hardware-tablet-wacom.nix
+    #./synergy-client.nix # barrier
 
-		./zramSwap.nix
+    ./hardware-printer.nix
+    ./hardware-tablet-wacom.nix
 
-		#./btrbk-pull.nix
-		#./btrbk-tv.nix # XXX: Temporarily disabled as the HDD is failing.
+    ./zramSwap.nix
 
-		./hosts2.nix
-		./configuration.FULL.nix
+    #./btrbk-pull.nix
+    #./btrbk-tv.nix # XXX: Temporarily disabled as the HDD is failing.
 
-		./kodi.nix
-    ];
+    ./hosts2.nix
+    ./configuration.FULL.nix
 
-    # For the value of 'networking.hostID', use the following command:
-    #     cksum /etc/machine-id | while read c rest; do printf "%x" $c; done
-    #
-    # custom desktop
-    networking.hostId = "7b2076ba";
-    networking.hostName = "customdesktop"; # "tv"; # tv.desktop.local
+    ./kodi.nix
+  ];
 
-    nix.settings.trusted-users = [ "root" "najib" ];
+  # For the value of 'networking.hostID', use the following command:
+  #     cksum /etc/machine-id | while read c rest; do printf "%x" $c; done
+  #
+  # custom desktop
+  networking.hostId = "7b2076ba";
+  networking.hostName = "customdesktop"; # "tv"; # tv.desktop.local
 
-    networking.useDHCP = false;
-    networking.interfaces.enp7s0.useDHCP = true;
-    #networking.interfaces.wlp3s0.useDHCP = true
-    #networking.interfaces.enp7s0.ipv4.addresses = [ {
-    #    address = "192.168.123.151";
-    #    prefixLength = 24;
-    #} ];
-    #networking.defaultGateway = "192.168.123.1";
-    # Refer network-dns.nix for DNS
-    #networking.enableIPv6 = false;
+  nix.settings.trusted-users = [ "root" "najib" ];
 
-    #boot.loader.systemd-boot.enable = true;
+  networking.useDHCP = false;
+  networking.interfaces.enp7s0.useDHCP = true;
+  #networking.interfaces.wlp3s0.useDHCP = true
+  #networking.interfaces.enp7s0.ipv4.addresses = [ {
+  #    address = "192.168.123.151";
+  #    prefixLength = 24;
+  #} ];
+  #networking.defaultGateway = "192.168.123.1";
+  # Refer network-dns.nix for DNS
+  #networking.enableIPv6 = false;
 
-    #boot.loader.efi.canTouchEfiVariables = true;
-    boot.loader.grub = {
-        enable = true;
-        version = 2;
-        #device = "/dev/sda"; #"nodev";
-        #efiSupport = true;
-        enableCryptodisk = true;
-		copyKernels = true;
-        #mirroredBoots = [
-            #{
-                #devices = [ "/dev/disk/by-id/wwn-0x5000cca7c5e11b3c" ];
-                #path = "/boot2";
-            #}
-        #];
-        #useOSProber = true;
-        devices = [
-			#"/dev/disk/by-id/ata-AGI256G06AI138_AGISAMUWK1011005-part1"
-			#"/dev/disk/by-id/wwn-0x5000c5001f67c049-part1"
-			#"/dev/sda1"
-			"/dev/sda"
-			#"3dacbf58-01"
-			#"/dev/disk/by-id/ata-AGI256G06AI138_AGISAMUWK1011005"
-        ];
-    };
+  #boot.loader.systemd-boot.enable = true;
 
-    boot.kernelPackages = pkgs.linuxPackages_latest;
-    #boot.supportedFilesystems = [ "ext4" "btrfs" "xfs" "vfat" ]; # "zfs" bcachefs
-    #boot.initrd.supportedFilesystems = [ "ext4" "btrfs" "xfs" "vfat" "dm-crypt" "dm-snapshot" "dm-raid" ]; # "zfs" bcachefs
-    #boot.loader.grub.copyKernels = true;
-
-    services.fstrim.enable = true;
-    hardware.enableAllFirmware = true;
-
-    # Needed for btrbk
-    services.openssh.permitRootLogin = "prohibit-password";
-
-    networking.firewall.enable = false;
-    # open port 24800 for barrier server?/client?
-
-    services.acpid.enable = true;
-    hardware.acpilight.enable = true;
-
-    # XXX
-    #environment.systemPackages = with pkgs; [
-    #    virtualboxWithExtpack
+  #boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.grub = {
+    enable = true;
+    version = 2;
+    #device = "/dev/sda"; #"nodev";
+    #efiSupport = true;
+    enableCryptodisk = true;
+    copyKernels = true;
+    #mirroredBoots = [
+      #{
+        #devices = [ "/dev/disk/by-id/wwn-0x5000cca7c5e11b3c" ];
+        #path = "/boot2";
+      #}
     #];
-    
-    boot.kernelModules = [ "snd-ctxfi" "snd-ca0106" "snd-hda-intel" ];
-    #boot.kernelModules = [ "snd-ctxfi" "snd-hda-intel" ];
+    #useOSProber = true;
+    devices = [
+      #"/dev/disk/by-id/ata-AGI256G06AI138_AGISAMUWK1011005-part1"
+      #"/dev/disk/by-id/wwn-0x5000c5001f67c049-part1"
+      #"/dev/sda1"
+      "/dev/sda"
+      #"3dacbf58-01"
+      #"/dev/disk/by-id/ata-AGI256G06AI138_AGISAMUWK1011005"
+    ];
+  };
 
-    # Test: Cuba disable, sebab SweetHome3D tak dapat jalan
-    #services.xserver.videoDrivers = [ "nvidiaLegacy390" ]; #"radeon" "cirrus" "vesa"  "vmware"  "modesetting" ];
-    #
-    #services.xserver.videoDrivers = [ "radeon" ];
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+  #boot.supportedFilesystems = [ "ext4" "btrfs" "xfs" "vfat" ]; # "zfs" bcachefs
+  #boot.initrd.supportedFilesystems = [ "ext4" "btrfs" "xfs" "vfat" "dm-crypt" "dm-snapshot" "dm-raid" ]; # "zfs" bcachefs
+  #boot.loader.grub.copyKernels = true;
 
-    #services.flatpak.enable = true;
-    #xdg.portal.enable = true;
-    #xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ]; # OR enable gnome desktopManager
+  services.fstrim.enable = true;
+  hardware.enableAllFirmware = true;
 
-    #services.xserver.displayManager.sddm.enable = true;
-    #services.xserver.displayManager.defaultSession = "none+xmonad";
-    #services.xserver.desktopManager.plasma5.enable = true;
-    #services.xserver.desktopManager.xfce.enable = true;
-    #services.xserver.desktopManager.enlightenment.enable = true;
+  # Needed for btrbk
+  services.openssh.permitRootLogin = "prohibit-password";
 
-    services.xserver.libinput.enable = true;
+  networking.firewall.enable = false;
+  # open port 24800 for barrier server?/client?
 
-    # Disable all power/screen saver; leave it to tv hardware
-    powerManagement.enable = false;
-    services.upower.enable = false;
-    powerManagement.powertop.enable = false;
-    services.tlp.enable = false;
-    services.auto-cpufreq = {
-	enable = true;
-    };
+  services.acpid.enable = true;
+  hardware.acpilight.enable = true;
 
-    networking.networkmanager.wifi.powersave = false;
-    systemd.watchdog.rebootTime = "10m";
+  # XXX
+  #environment.systemPackages = with pkgs; [
+  #    virtualboxWithExtpack
+  #];
 
-    #nix.maxJobs = 4;
+  #boot.kernelModules = [ "snd-ctxfi" "snd-ca0106" "snd-hda-intel" ];
+  #boot.kernelModules = [ "snd-ctxfi" "snd-hda-intel" ];
 
-	environment.systemPackages = [
-		pkgs.blender
-	];
+  # Test: Cuba disable, sebab SweetHome3D tak dapat jalan
+  #services.xserver.videoDrivers = [ "nvidiaLegacy390" ]; #"radeon" "cirrus" "vesa"  "vmware"  "modesetting" ];
+  #
+  #services.xserver.videoDrivers = [ "radeon" ];
+
+  #services.flatpak.enable = true;
+  #xdg.portal.enable = true;
+  #xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ]; # OR enable gnome desktopManager
+
+  #services.xserver.displayManager.sddm.enable = true;
+  #services.xserver.displayManager.defaultSession = "none+xmonad";
+  #services.xserver.desktopManager.plasma5.enable = true;
+  #services.xserver.desktopManager.xfce.enable = true;
+  #services.xserver.desktopManager.enlightenment.enable = true;
+
+  services.xserver.libinput.enable = true;
+
+  # Disable all power/screen saver; leave it to tv hardware
+  powerManagement.enable = false;
+  services.upower.enable = false;
+  powerManagement.powertop.enable = false;
+  services.tlp.enable = false;
+  services.auto-cpufreq = {
+    enable = true;
+  };
+
+  networking.networkmanager.wifi.powersave = false;
+  systemd.watchdog.rebootTime = "10m";
+
+  #nix.maxJobs = 4;
+
+  environment.systemPackages = [
+    pkgs.blender
+  ];
 }
