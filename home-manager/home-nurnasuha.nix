87067191d03ceb1869fe5ce768690bcf3c6c49b7
Nur Nasuha <nurnasuhabintimohdnajib@gmail.com>
2023-01-31 13:40:24 +0800

    separate config git-username and git-useremail for each user

--- home-manager/home-nurnasuha.nix
+++ home-manager/home-nurnasuha.nix
@@ -57,9 +57,9 @@ in
   };
 
   # Add stuff for your user as you see fit:
-  
+
   #programs.neovim.enable = true;
-  
+
   home.packages = with pkgs; [
     posterazor
     remmina
@@ -84,7 +84,13 @@ in
 
   # Enable home-manager and git
   programs.home-manager.enable = true;
-  programs.git.enable = true;
+  programs.git = {
+    enable = true;
+    userName = "Nur Nasuha";
+    userEmail = "nurnasuhabintimohdnajib@gmail.com";
+    #cincludes.
+  };
+  programs.gitui.enable = true;
 
   # Nicely reload system units when changing configs
   systemd.user.startServices = "sd-switch";
