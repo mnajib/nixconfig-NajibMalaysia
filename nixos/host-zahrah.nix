ed880103e2a53a5c83a0212ee33d1b1d3a99ee4a
Muhammad Na'im <muhammadnaimbinmohdnajib@gmail.com>
2023-02-06 11:09:17 +0800

    change display manager from gdm to lightdm for zahrah

--- nixos/host-zahrah.nix
+++ nixos/host-zahrah.nix
@@ -232,9 +232,9 @@
   services.xserver.libinput.touchpad.tapping = true; #false;
 
   #services.xserver.displayManager.sddm.enable = true;
-  services.xserver.displayManager.gdm.enable = true;
+  #services.xserver.displayManager.gdm.enable = true;
   services.xserver.displayManager.startx.enable = true;
-  #services.xserver.displayManager.lightdm.enable = true;
+  services.xserver.displayManager.lightdm.enable = true;
 
   services.xserver.displayManager.defaultSession = "none+xmonad";
   #services.xserver.desktopManager.plasma5.enable = true;
