7c3d75d3d385ced96a87b0f7aec36316077fefb1
Najib Ibrahim <mnajib@gmail.com>
2023-06-08 19:25:32 +0800

    nfs-client-automount.nix for each user home dir

--- nixos/nfs-client-automount.nix
+++ nixos/nfs-client-automount.nix
@@ -28,8 +28,26 @@
   #  ];
   #};
 
-  fileSystems."/mnt/documents" = {
-    device = "mahirah:/documents";
+  #fileSystems."/mnt/documents" = {
+  #  device = "mahirah:/documents";
+  #  fsType = "nfs";
+  #  options = [
+  #    #"nfsvers=3"
+  #    #"nfsvers=4.2"
+  #
+  #    # Lazy-mounting: By default, all shares will be mounted right when your machine starts - apart from being simply unwanted sometimes, this may also cause issues when your computer doesn't have a stable network connection or uses WiFi; you can fix this by telling systemd to mount your shares the first time they are accessed (instead of keeping them mounted at all times): 
+  #    "x-systemd.automount" "noauto"
+  #
+  #    # Disconnects after 10 minutes (i.e. 600 seconds)
+  #    "x-systemd.idle-timeout=600"
+  #
+  #    # XXX:
+  #    "noatime"
+  #  ];
+  #};
+
+  fileSystems."/mnt/nfsshare2" = {
+    device = "customdesktop:/nfsshare2";
     fsType = "nfs";
     options = [
       #"nfsvers=3"
@@ -46,20 +64,67 @@
     ];
   };
 
-  fileSystems."/mnt/nfsshare2" = {
-    device = "customdesktop:/nfsshare2";
+  # mkdir -p /mnt/home/najib
+  fileSystems."/mnt/home/najib" = {
+    device = "customdesktop:/najib";
     fsType = "nfs";
     options = [
       #"nfsvers=3"
       #"nfsvers=4.2"
+      "x-systemd.automount" "noauto"      # Lazy-mounting: By default, all shares will be mounted right when your machine starts - apart from being simply unwanted sometimes, this may also cause issues when your computer doesn't have a stable network connection or uses WiFi; you can fix this by telling systemd to mount your shares the first time they are accessed (instead of keeping them mounted at all times): 
+      "x-systemd.idle-timeout=600"        # Disconnects after 10 minutes (i.e. 600 seconds)
+      "noatime"
+    ];
+  };
 
-      # Lazy-mounting: By default, all shares will be mounted right when your machine starts - apart from being simply unwanted sometimes, this may also cause issues when your computer doesn't have a stable network connection or uses WiFi; you can fix this by telling systemd to mount your shares the first time they are accessed (instead of keeping them mounted at all times): 
-      "x-systemd.automount" "noauto"
+  # mkdir -p /mnt/home/julia
+  fileSystems."/mnt/home/julia" = {
+    device = "customdesktop:/julia";
+    fsType = "nfs";
+    options = [
+      #"nfsvers=3"
+      #"nfsvers=4.2"
+      "x-systemd.automount" "noauto"      # Lazy-mounting: By default, all shares will be mounted right when your machine starts - apart from being simply unwanted sometimes, this may also cause issues when your computer doesn't have a stable network connection or uses WiFi; you can fix this by telling systemd to mount your shares the first time they are accessed (instead of keeping them mounted at all times): 
+      "x-systemd.idle-timeout=600"        # Disconnects after 10 minutes (i.e. 600 seconds)
+      "noatime"
+    ];
+  };
 
-      # Disconnects after 10 minutes (i.e. 600 seconds)
-      "x-systemd.idle-timeout=600"
+  # mkdir -p /mnt/home/naqib
+  fileSystems."/mnt/home/naqib" = {
+    device = "customdesktop:/naqib";
+    fsType = "nfs";
+    options = [
+      #"nfsvers=3"
+      #"nfsvers=4.2"
+      "x-systemd.automount" "noauto"      # Lazy-mounting: By default, all shares will be mounted right when your machine starts - apart from being simply unwanted sometimes, this may also cause issues when your computer doesn't have a stable network connection or uses WiFi; you can fix this by telling systemd to mount your shares the first time they are accessed (instead of keeping them mounted at all times): 
+      "x-systemd.idle-timeout=600"        # Disconnects after 10 minutes (i.e. 600 seconds)
+      "noatime"
+    ];
+  };
 
-      # XXX:
+  # mkdir -p /mnt/home/nurnasuha
+  fileSystems."/mnt/home/nurnasuha" = {
+    device = "customdesktop:/nurnasuha";
+    fsType = "nfs";
+    options = [
+      #"nfsvers=3"
+      #"nfsvers=4.2"
+      "x-systemd.automount" "noauto"      # Lazy-mounting: By default, all shares will be mounted right when your machine starts - apart from being simply unwanted sometimes, this may also cause issues when your computer doesn't have a stable network connection or uses WiFi; you can fix this by telling systemd to mount your shares the first time they are accessed (instead of keeping them mounted at all times): 
+      "x-systemd.idle-timeout=600"        # Disconnects after 10 minutes (i.e. 600 seconds)
+      "noatime"
+    ];
+  };
+
+  # mkdir -p /mnt/home/naim
+  fileSystems."/mnt/home/naim" = {
+    device = "customdesktop:/naim";
+    fsType = "nfs";
+    options = [
+      #"nfsvers=3"
+      #"nfsvers=4.2"
+      "x-systemd.automount" "noauto"      # Lazy-mounting: By default, all shares will be mounted right when your machine starts - apart from being simply unwanted sometimes, this may also cause issues when your computer doesn't have a stable network connection or uses WiFi; you can fix this by telling systemd to mount your shares the first time they are accessed (instead of keeping them mounted at all times): 
+      "x-systemd.idle-timeout=600"        # Disconnects after 10 minutes (i.e. 600 seconds)
       "noatime"
     ];
   };
