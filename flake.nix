c5745326f3bd336f8b80110bbdc31da5d5d9b9ea
Najib Ibrahim <mnajib@gmail.com>
2023-10-14 21:22:57 +0800

    disable nixConfig experimental-features and extra-substituters; because too slow, not sure why

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Najib new nix flake config";
 
   nixConfig = {
-    experimental-features = [ "nix-command" "flakes" ];
+    #experimental-features = [ "nix-command" "flakes" ];
 
     #substituters = [
     #  # Replace the official cache with a mirror located in China
@@ -10,14 +10,14 @@
     #  "https://cache.nixos.org/"
     #];
 
-    extra-substituters = [
-      # Nix community's cache server
-      "https://nix-community.cachix.org"
-    ];
+    #extra-substituters = [
+    #  # Nix community's cache server
+    #  "https://nix-community.cachix.org"
+    #];
 
-    extra-trusted-public-keys = [
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-    ];
+    #extra-trusted-public-keys = [
+    #  "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    #];
   };
 
   inputs = {
