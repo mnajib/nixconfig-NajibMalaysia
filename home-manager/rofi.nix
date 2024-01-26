8d4ef2d95fa9ccc2594533b9190972f1d2b5e049
Juliani Jaffar <juliani.jaffar@gmail.com>
2024-01-26 11:54:49 +0800

    try fix rofi config

--- home-manager/rofi.nix
+++ home-manager/rofi.nix
@@ -1,10 +1,46 @@
 {
   config,
+  pkgs,
+  inputs,
+  outputs,
+  lib,
   ...
 }:
 {
+  #home.packages = with pkgs; [
+  #  rofi
+  #];
+
+  programs.rofi = {
+    enable = true;
+
+    package = pkgs.rofi.override {
+      plugins = with pkgs; [
+        rofi-file-browser
+        rofi-pass
+        rofi-calc
+        rofi-emoji
+        rofi-rbw
+        rofi-rbw-x11
+        rofi-systemd
+        rofi-screenshot
+        rofi-power-menu
+        rofi-pulse-select
+      ];
+    };
+
+  };
+
   home.file."./config/rofi" = {
     source = ./src/.config/rofi;
     recursive = true;
   };
+  #home.file."./config/rofi" = {
+  #  source = ./src/.config/rofi;
+  #  recursive = true;
+  #};
+  #home.file."./config/rofi" = {
+  #  source = ./src/.config/rofi;
+  #  recursive = true;
+  #};
 }
