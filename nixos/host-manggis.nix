f1bb93a7854b066d16694cb7343cd83dd878c0d3
Julia <juliani.jaffar@gmail.com>
2024-02-01 08:37:10 +0800

    flatpak and blender for manggis; update flakes

--- nixos/host-manggis.nix
+++ nixos/host-manggis.nix
@@ -27,6 +27,7 @@
         #./users-anak2.nix
 
         ./nfs-client.nix
+        ./flatpak.nix
 
         # Keyboard for console:
         #./console-keyboard-dvorak.nix
@@ -49,6 +50,10 @@
         ./configuration.FULL.nix
 
         ./typesetting.nix
+    ];
+
+    environment.systemPackages = with pkgs; [
+        blender
     ];
 
     # For the value of 'networking.hostID', use the following command:
