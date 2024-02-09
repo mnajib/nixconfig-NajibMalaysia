80415045e9b2aad684b17b46dbe2fb0ae97f4624
Juliani Jaffar <juliani.jaffar@gmail.com>
2024-02-10 01:31:20 +0800

    some changes in flake.nix when updating keira

--- flake.nix
+++ flake.nix
@@ -1,3 +1,9 @@
+#
+# NOTES:
+#
+#  nix flake show
+#
+
 {
   description = "Najib new NixOS configuration with flakes";
 
@@ -25,7 +31,7 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     #nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
     #
-    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixpkgs-master.url = "github:nixos/nixpkgs/master";
     nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-23.05";
 
@@ -34,8 +40,10 @@
     #nixpkgs-najib.url = "github:/mnajib/nixpkgs/najib";
 
     # Home manager
-    home-manager.url = "github:nix-community/home-manager";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
     # TODO: Add any other flake you might need
     #hardware.url = "github:nixos/nixos-hardware";
@@ -77,11 +85,13 @@
     self,
     nixpkgs,
     nixpkgs-stable,
+    nixpkgs-unstable,
     #nixpkgs-najib,
     home-manager,
     hardware,
     flake-utils,
     nur,
+    impermanence,
     nix-colors,
     hyprland,
     #sile,
@@ -101,6 +111,12 @@
         "aarch64-darwin"
         "x86_64-darwin"
       ];
+      pkgs = import nixpkgs {
+        config.allowUnfree = true;
+      };
+      pkgsUnstable = import nixpkgs-unstable {
+        config.allowUnfree = true;
+      };
     in
     rec {
 
@@ -160,6 +176,7 @@
         # Laptop Thinkpad X230
         khawlah = nixpkgs.lib.nixosSystem {
           specialArgs = { inherit inputs outputs; };
+          system = "x86_64-linux";
           modules = [
             #./nixos/configuration-khawlah.nix
             ./nixos/host-khawlah.nix
@@ -169,6 +186,8 @@
         # Laptop Dell Najib
         khadijah = nixpkgs.lib.nixosSystem {
           specialArgs = { inherit inputs outputs; };
+
+          #system = "x86_64-linux";
 
           modules = [
             # system-wide doom-emacs
@@ -188,8 +207,23 @@
             #  doom-emacs
             #];
 
-            #
+            #./machines/host-khadijah.nix
             ./nixos/host-khadijah.nix
+
+            #pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
+            #extraSpecialArgs = { inherit inputs outputs; };
+            #home-manager.nixosModules.home-manager {
+            #  home-manager = {
+            #    useGlobalPkgs = true;
+            #    useUserPackages = true;
+            #    users.najib = import ./home-manager/home-najib.nix {
+            #      inherit pkgs;
+            #      inherit pkgsUnstable;
+            #      inherit impermanence;
+            #      inherit nur;
+            #    };
+            #  };
+            #}
           ];
         };
 
@@ -280,12 +314,33 @@
         # Laptop Thinkpad T410 (without nvidia) Julia
         keira = nixpkgs.lib.nixosSystem {
           specialArgs = { inherit inputs outputs; };
+          system = "x86_64-linux";
+          #system = nixpkgs.hostPlatform;
           modules = [
             ./nixos/host-keira.nix
 
             # Add your model from this list:
             # http://github.com/NixOS/nixos-hardware/blob/master/flake.nix
             hardware.nixosModules.lenovo-thinkpad-t410
+
+            #home-manager.nixosModules.home-manager {
+            #  home-manager = {
+            #    useGlobalPkgs = true;
+            #    useUserPackages = true;
+            #    users.najib = import ./home-manager/home-najib.nix {
+            #      inherit pkgs;
+            #      inherit pkgsUnstable;
+            #      inherit impermanence;
+            #      inherit nur;
+            #    };
+            #    users.julia = import ./home-manager/home-julia.nix {
+            #      inherit pkgs;
+            #      inherit pkgsUnstable;
+            #      inherit impermanence;
+            #      inherit nur;
+            #    };
+            #  };
+            #}
           ];
         };
 
