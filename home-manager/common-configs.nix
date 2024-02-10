3968e18706060f96304bb1dfd6b69e8ac41bcd44
Muhammad Naqib <m.naqib.bin.m.najib@gmail.com>
2024-02-10 10:22:01 +0800

    fix some error reported on qutebrowser startup

--- home-manager/common-configs.nix
+++ home-manager/common-configs.nix
@@ -156,17 +156,17 @@ in
 
     #extraConfig = builtins.readFile ./src/.config/nvim/init.vim;
 
+      #c.colors.webpage.darkmode.grayscale.images = 0.35
+      #c.content.user_stylesheets = '~/.config/qutebrowser/stylesheet/mydarkmodefix.css'
     extraConfig = ''
       c.colors.webpage.preferred_color_scheme = 'dark'
-      c.colors.webpage.darkmode.enable = True
+      c.colors.webpage.darkmode.enabled = True
       c.colors.webpage.darkmode.algorithm = 'lightness-hsl'
       c.colors.webpage.darkmode.contrast = -.022
-      c.colors.webpage.darkmode.threshold.text = 150
+      c.colors.webpage.darkmode.threshold.foreground = 150
       c.colors.webpage.darkmode.threshold.background = 100
       c.colors.webpage.darkmode.policy.images = 'never'
-      c.colors.webpage.darkmode.grayscale.images = 0.35
       c.content.notifications.enabled = False
-      c.content.user_stylesheets = '~/.config/qutebrowser/stylesheet/mydarkmodefix.css'
     '';
   };
   #home.file.".config/qutebrowser/stylesheet/mydarkmodefix.css" = {
