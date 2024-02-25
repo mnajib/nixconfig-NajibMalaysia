fc63001bcda7900a3e8b0b881643f635ba0404a9
Julia <juliani.jaffar@gmail.com>
2024-02-25 21:46:10 +0800

    update users-naqib-wheel.nix

--- nixos/users-naqib-wheel.nix
+++ nixos/users-naqib-wheel.nix
@@ -1,16 +1,16 @@
 {
-    users.users.naqib = {
-        description = "Muhammad Naqib Bin Muhammad Najib";
-        uid = 1003;
-        isNormalUser = true;
-        #initialPassword = "password";
-        createHome = true;
-        home = "/home/naqib";
-        extraGroups = [
-		"networkmanager" "audio" "video" "cdrom" "scanner" "lp" "systemd-journal"
-		"istana46" "naqib"
-		"dialout"
-		"wheel"
-	];
-    };
+  users.users.naqib = {
+    description = "Muhammad Naqib Bin Muhammad Najib";
+    uid = 1003;
+    isNormalUser = true;
+    #initialPassword = "password";
+    createHome = true;
+    home = "/home/naqib";
+    extraGroups = [
+      "networkmanager" "audio" "video" "cdrom" "scanner" "lp" "systemd-journal"
+      "istana46" "naqib"
+      "dialout"
+      "wheel"
+    ];
+  };
 }
