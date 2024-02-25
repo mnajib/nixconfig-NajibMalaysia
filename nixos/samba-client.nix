48fb50187169d92446b9fdd28c0eb528c6667aa8
Najib Ibrahim <mnajib@gmail.com>
2024-02-25 20:50:37 +0800

    enable samba server and client for customdesktop

--- nixos/samba-client.nix
+++ nixos/samba-client.nix
@@ -41,4 +41,17 @@
   #      "${automount_opts},credentials=/etc/nixos/smb-secrets"
   #    ];
   #};
+
+  fileSystems."/home/nfs/share/DATA" = {
+    device = "//customdesktop/data";
+    fsType = "cifs";
+    options = [
+      "noauto"
+      "x-systemd.automount"
+      "x-systemd.idle-timeout=60"
+      "x-systemd.device-timeout=5s"
+      "x-systemd.mount-timeout=5s"
+    ];
+  };
+
 }
