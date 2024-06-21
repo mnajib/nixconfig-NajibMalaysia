f15dd052d2cc60f14b42c6309689903a9d2a45fd
Naqib Najib <m.naqib.bin.m.najib@gmail.com>
2024-06-21 16:02:29 +0800

    re-enable gitea service in customdesktop with restore data from backup

--- nixos/gitea.nix
+++ nixos/gitea.nix
@@ -1,3 +1,10 @@
+#
+# NOTES:
+#   systemctl
+#   systemctl status gitea.service
+#   netstat -tulpen | grep '3000'
+#
+
 {
   nixpkgs,
   config,
@@ -13,10 +20,25 @@
       #server.ROOT_URL = "http://mahirah:3000/";
       #server.ROOT_URL = "http://192.168.1.72:3000/";
     };
-    stateDir = "/var/lib/gitea";                                              # default
+    #stateDir = "/var/lib/gitea";                                              # default
+    stateDir = "/mnt/data/gitea";                                              # default
     customDir = "${config.services.gitea.stateDir}/custom";                   # default
     repositoryRoot = "${config.services.gitea.stateDir}/repositories";        # default
     #lfs.contentDir = ...
     #dump.backupDir = ...
+
+    #options = [
+    #  "x-systemd.after-mnt-data.automount"
+    #];
   };
+
+  systemd.services.gitea = {
+    after = [
+      "mnt-data.automount"
+    ];
+  };
+
+  networking.firewall.allowedTCPPorts = [
+    3000	# gitea
+  ];
 }
