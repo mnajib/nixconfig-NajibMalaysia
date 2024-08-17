4eba553091ba1f6d01aed4113ae1a6a033e0ba2f
Muhammad Na'im <muhammadnaimbinmohdnajib@gmail.com>
2024-08-17 16:50:23 +0800

    install pulseaudio for pactl command

--- nixos/audio-pipewire.nix
+++ nixos/audio-pipewire.nix
@@ -1,22 +1,27 @@
+{ pkgs, config, ... }:
 {
-	# Remove sound.enable or turn it off if you had it set previously, it seems to cause conflicts with pipewire
-	#sound.enable = false;
-	sound.enable = true;
+  # Remove sound.enable or turn it off if you had it set previously, it seems to cause conflicts with pipewire
+  #sound.enable = false;
+  sound.enable = true;
 
-	hardware.pulseaudio.enable = false;
+  hardware.pulseaudio.enable = false;
 
-	# rtkit is optional but recommended
-	security.rtkit.enable = true;
-	services.pipewire = {
-		enable = true;
-		alsa.enable = true;
-		alsa.support32Bit = true;
-		pulse.enable = true;
-		# If you want to use JACK applications, uncomment this
-		jack.enable = true;
+  # rtkit is optional but recommended
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    jack.enable = true;
 
-		# use the example session manager (no others are packaged yet so this is enabled by default,
-		# no need to redefine it in your config for now)
-		#media-session.enable = true;
-	};
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  environment.systemPackages = with pkgs; [
+    pulseaudioFull
+  ];
 }
