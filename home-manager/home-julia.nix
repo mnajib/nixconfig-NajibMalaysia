8d019753370a0d9ad7ca7728d577f3a806e939b6
Julia <juliani.jaffar@gmail.com>
2024-08-26 09:35:03 +0800

    add some packages for home-manager julia

--- home-manager/home-julia.nix
+++ home-manager/home-julia.nix
@@ -61,16 +61,35 @@ in
   #programs.neovim.enable = true;
 
   home.packages = with pkgs; [
-    posterazor
-    remmina
-    wpsoffice
-    clamav
+    posterazor                          # for print on big paper?
+    remmina                             # RDP remote desktop client ?
+    wpsoffice                           #
+    clamav                              # scan anti-virus ?
+    mindustry                           # game
+    minetest                            # game
     #audacity
     #shotcut
-    #calligra        # calligra marked unsecure because using qtwebkit
+    #calligra                           # calligra marked unsecure because using qtwebkit
     #openshot-qt
     #kmymoney
     #shotwell
+    gv                                  # view and navigate through PostScript and PDF documents on an X display (user interface for the ghostscript interpreter)
+
+    gnuchess                            # chess-engine
+    stockfish                           # strong open source chess-engine
+    fairymax                            # a small chess-engine supporting fairy pieces
+    xboard                              # gui for chess-engine
+    eboard                              # chess interface for unix-like systems
+    gnome.gnome-chess                   # gui chess game
+    #kdePackages.knights                 # Chess board program
+    cutechess                           # GUI, CLI, and library for playing chess
+    uchess                              # play chess against UCI engines in your terminal
+    gambit-chess                        # play chess in your terminal
+    arena                               # Chess GUI for analyzing with and playing against various engines
+    chessx                              # browse and analyse chess games
+    chessdb                             # a free chess database
+
+    gshogi                              # a GUI implementation of the Shogi board game (also known as Japanese Chess)
   ];
 
   # Enable home-manager and git
