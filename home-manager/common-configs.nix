4edec05ab1faa0ae5cd67b737a96b46a0f016eec
Muhammad Na'im <muhammadnaimbinmohdnajib@gmail.com>
2024-08-26 20:14:44 +0800

    disable darkmode for qutebrowser home-manager common-config.nix

--- home-manager/common-configs.nix
+++ home-manager/common-configs.nix
@@ -168,16 +168,16 @@ in
 
       #c.colors.webpage.darkmode.grayscale.images = 0.35
       #c.content.user_stylesheets = '~/.config/qutebrowser/stylesheet/mydarkmodefix.css'
-    extraConfig = ''
-      c.colors.webpage.preferred_color_scheme = 'dark'
-      c.colors.webpage.darkmode.enabled = True
-      c.colors.webpage.darkmode.algorithm = 'lightness-hsl'
-      c.colors.webpage.darkmode.contrast = -.022
-      c.colors.webpage.darkmode.threshold.foreground = 150
-      c.colors.webpage.darkmode.threshold.background = 100
-      c.colors.webpage.darkmode.policy.images = 'never'
-      c.content.notifications.enabled = False
-    '';
+    #extraConfig = ''
+    #  c.colors.webpage.preferred_color_scheme = 'dark'
+    #  c.colors.webpage.darkmode.enabled = True
+    #  c.colors.webpage.darkmode.algorithm = 'lightness-hsl'
+    #  c.colors.webpage.darkmode.contrast = -.022
+    #  c.colors.webpage.darkmode.threshold.foreground = 150
+    #  c.colors.webpage.darkmode.threshold.background = 100
+    #  c.colors.webpage.darkmode.policy.images = 'never'
+    #  c.content.notifications.enabled = False
+    #'';
   };
   #home.file.".config/qutebrowser/stylesheet/mydarkmodefix.css" = {
   #home.file."mydarkmodefix.css" = {
