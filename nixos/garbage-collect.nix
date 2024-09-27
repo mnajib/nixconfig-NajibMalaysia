383ce4955196f86c7672a072b41b2f43a24c9c1a
Muhammad Naqib <m.naqib.bin.m.najib@gmail.com>
2024-09-27 18:36:45 +0800

    fix duplicate file for same solution

--- nixos/garbage-collect.nix
+++ nixos/garbage-collect.nix
@@ -1,5 +1,1 @@
-{
-  nix.gc.automatic = true;          # Enable automatic garbage collection
-  nix.gc.dates = "weekly";          # Set GC to run weekly
-  nix.gc.options = "--delete-older-than 30d";  # Keep only generations younger than 30 days
-}
+nix-garbage-collector.nix
