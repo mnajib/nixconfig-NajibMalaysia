755cd737245f6d4a754830240ae2de7aab7a349c
Najib Ibrahim <mnajib@gmail.com>
2024-10-20 09:23:09 +0800

    gtk as lightdm.greeter insteed of slick

--- nixos/host-khadijah.nix
+++ nixos/host-khadijah.nix
@@ -363,12 +363,25 @@ with lib;
 
       lightdm = {
         enable = true;
+        #background = "";
         greeters = {
-          gtk = {
-            enable = false;
+          gtk = { # gtk is the default
+            enable = true;
+            theme = "Adwaita";
+            indicators = [
+              "~host"
+              "~spacer"
+              "~clock"
+              "~spacer"
+              "~session"
+              "~language"
+              "~a11y"
+              "~power"
+            ];
+            clock-format = "%F";
           };
           slick = {
-            enable = true;
+            enable = false;
           };
           enso.enable = false;
         };
