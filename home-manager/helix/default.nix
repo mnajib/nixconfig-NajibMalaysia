47ad5d044b527cd04f99dd11fb4caa9b3af3f68a
Najib Ibrahim <mnajib@gmail.com>
2024-11-14 16:43:27 +0800

    configure helix (text editor)

--- home-manager/helix/default.nix
+++ home-manager/helix/default.nix
@@ -15,22 +15,54 @@ in
   ];
 
   programs.helix.enable = true;
-  #programs.helix.defaultEditor = true;
+  programs.helix.defaultEditor = false;
+
+  programs.helix.package = pkgs.helix;
+  #programs.helix.package = pkgs.evil-helix;
 
   programs.helix.extraPackages = with pkgs; [
     marksman
     helix-gpt
-    #evil-helix
     vim-language-server
     haskell-language-server
     bash-language-server
   ];
 
-  programs.helix.ignores = [];
+  programs.helix.ignores = [
+    ".build/"
+    "!.gitignore"
+  ];
 
   programs.helix.languages = {};
 
-  programs.helix.settings = {};
+  programs.helix.settings = {
+    editor = {
+      # export COLORTERM=truecolor
+      true-color = true;
+
+      #whitespace.render = "all";
+      whitespace.render = {
+        space = "all";
+        tab = "all";
+        nbsp = "none";
+        nnbsp = "none";
+        newline = "none";
+      };
+      #whitespace.characters = {
+      #  space = "·";
+      #  nbsp = "⍽";
+      #  nnbsp = "␣";
+      #  tab = "→";
+      #  newline = "⏎";
+      #  tabpad = "·";
+      #};
+
+      indent-guides = {
+        render = true;
+      };
+
+    }; # End programs.helix.editor
+  };
 
   programs.helix.themes = {};
 
