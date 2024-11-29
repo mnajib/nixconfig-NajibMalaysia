33f9a2c54fc59e51095084666d2b68f9a63ec9db
Najib Ibrahim <mnajib@gmail.com>
2024-11-29 21:34:55 +0800

    stylix.fonts.size to smaller size (9)

--- nixos/stylix.nix
+++ nixos/stylix.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 {
 
   stylix.enable = true;
@@ -40,35 +40,38 @@
   #---------------------------------------------------------------------------
   stylix.fonts = {
     monospace = {
-      package = pkgs.nerdfonts.override {fonts = [ "JetBrainsMono" ];};
-      name = "JetBrainsMono Nerd Font Mono";
+      #package = pkgs.nerdfonts.override {fonts = [ "JetBrainsMono" ];};
+      #name = "JetBrainsMono Nerd Font Mono";
+      #package = pkgs.powerline-fonts;
+      package = pkgs.nerdfonts.override { fonts = [ "FiraMono" ]; };
+      name = "Fira Mono for Powerline (Bold)";
     };
     #monospace = {
     #  package = pkgs.dejavu_fonts;
     #  name = "DejaVu Sans Mono";
     #};
-    sansSerif = {
-      package = pkgs.dejavu_fonts;
-      name = "DejaVu Sans";
-    };
-    serif = {
-      package = pkgs.dejavu_fonts;
-      name = "DejaVu Serif";
-    };
-    emoji = {
-      package = pkgs.noto-fonts-emoji;
-      name = "Noto Color Emoji";
-    };
-    #serif = config.stylix.fonts.monospace;
-    #sansSerif = config.stylix.fonts.monospace;
-    #emoji = config.stylix.fonts.monospace;
+    #sansSerif = {
+    #  package = pkgs.dejavu_fonts;
+    #  name = "DejaVu Sans";
+    #};
+    #serif = {
+    #  package = pkgs.dejavu_fonts;
+    #  name = "DejaVu Serif";
+    #};
+    #emoji = {
+    #  package = pkgs.noto-fonts-emoji;
+    #  name = "Noto Color Emoji";
+    #};
+    serif = config.stylix.fonts.monospace;
+    sansSerif = config.stylix.fonts.monospace;
+    emoji = config.stylix.fonts.monospace;
   };
 
   stylix.fonts.sizes = {
-    applications = 12; # 1
-    terminal = 10; # 15;
-    desktop = 10; # 10;
-    popups = 10; # 10;
+    applications = 9;#12; # 1
+    terminal = 9;#10; # 15;
+    desktop = 9;#10; # 10;
+    popups = 9;#10; # 10;
   };
 
   #stylix.opacity = {
