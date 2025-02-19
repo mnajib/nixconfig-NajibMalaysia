e3b5e28356af3f416951ed53a151f6d218620cd1
Najib Ibrahim <mnajib@gmail.com>
2025-02-19 20:52:41 +0800

    some CSV related tools

--- nixos/configuration.FULL.nix
+++ nixos/configuration.FULL.nix
@@ -217,6 +217,32 @@
     socat
     redis
 
+    csvq
+    csvtk
+    csvkit
+    csview
+    csv2md
+    csvtool
+    csvlens
+    csvdiff
+    csv2svg
+    csv2odf
+    csv-tui
+    csvquote
+    xlsx2csv
+    qsv
+    xsv
+    clevercsv
+    graph-cli
+    zsv
+    textql
+    tabview
+    tidy-viewer
+    tabiew
+    miller
+    json-plot
+    clevercsv
+
     unixODBC
     unixODBCDrivers.sqlite
     unixODBCDrivers.psql
