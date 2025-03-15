875f4b3a725df499b6333eef6efcefda42e4a476
Najib Ibrahim <mnajib@gmail.com>
2025-03-15 15:12:23 +0800

    nixos: printer: initial test for sharing new printer on nyxora

--- nixos/hardware-printer.nix
+++ nixos/hardware-printer.nix
@@ -1,12 +1,26 @@
 { pkgs, config, ... }:
 {
-	# Enable CUPS to print docoments.
-	services.printing.enable = true;
-	services.printing.browsing = true;
-	services.printing.defaultShared = false;
-	services.printing.drivers = with pkgs; [ gutenprint hplip splix ];
-	services.avahi.enable = false;
+  # Enable CUPS to print docoments.
+  services.printing.enable = true;
+  services.printing.browsing = true;
+  services.printing.defaultShared = false;
+  services.printing.drivers = with pkgs; [ gutenprint hplip splix ];
 
-	#services.avahi.nssmdns = false;
-	services.avahi.nssmdns4 = false;
+  services.avahi.enable = false;
+  ##services.avahi.nssmdns = false;
+  services.avahi.nssmdns4 = false;
+  #
+  #services.ipp-usb.enable = true;
+
+  hardware.sane.enable = true;
+
+  services.system-config-printer.enable = true;
+  programs.system-config-printer.enable = true;
+
+  services.printing.browsed.enable = true;
+
+  networking.firewall = {
+    allowedTCPPorts = [ 631 ];
+    allowedUDPPorts = [];
+  };
 }
