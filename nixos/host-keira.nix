ea104a99f13db85aa5db128af1948b1da46b0485
Juliani Jaffar <juliani.jaffar@gmail.com>
2025-05-12 20:32:48 +0800

    services.btrfs.autoScrub.interval to daily, start ad 03:00

--- nixos/host-keira.nix
+++ nixos/host-keira.nix
@@ -134,8 +134,8 @@
     #interval = "monthly";
     #interval = "weekly";
     #interval = "daily";
-    #interval = "*-*-* 03:00:00"; # Daily, start at 03:00:00 ?
-    interval = "*-*-*/2 03:00:00"; # ... every two days, at 03:00:00 ?
+    interval = "*-*-* 03:00:00"; # Daily, start at 03:00:00 ?
+    #interval = "*-*-*/2 03:00:00"; # ... every two days, at 03:00:00 ?
   };
 
   services.fstrim.enable = true;
