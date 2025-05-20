aed1b25e81483b8ab306f8e3e1cb752726a7b52f
Najib Ibrahim <mnajib@gmail.com>
2025-05-21 06:33:17 +0800

    kitty

--- home-manager/common-configs.nix
+++ home-manager/common-configs.nix
@@ -122,6 +122,7 @@ in
 
   programs.kitty = {
     enable = true;
+    #theme = "Space Gray Eighties";
 
     #font = {
       #package = pkgs.dejavu_fonts;
@@ -130,11 +131,51 @@ in
       #size = "12"; #"13"; # "8";
     #};
 
-    #theme = "Space Gray Eighties";
+    font = {
+      name = "Hack Nerd Font";  # Ensure Nerd Fonts are installed
+      size = 8.0;
+    };
 
     settings = {
-      background = "#${config.colorScheme.palette.base00}";
-      foreground = "#${config.colorScheme.palette.base05}";
+      #background = "#${config.colorScheme.palette.base00}";
+      #foreground = "#${config.colorScheme.palette.base05}";
+
+      #window_padding_width = 15;
+      foreground = "#${colors.base05}";
+      background = "#${colors.base00}";
+      selection_background = "#${colors.base05}";
+      selection_foreground = "#${colors.base00}";
+      url_color = "#${colors.base04}";
+      cursor = "#${colors.base05}";
+      active_border_color = "#${colors.base03}";
+      inactive_border_color = "#${colors.base01}";
+      active_tab_background = "#${colors.base00}";
+      active_tab_foreground = "#${colors.base05}";
+      inactive_tab_background = "#${colors.base01}";
+      inactive_tab_foreground = "#${colors.base04}";
+      tab_bar_background = "#${colors.base01}";
+      color0 = "#${colors.base00}";
+      color1 = "#${colors.base08}";
+      color2 = "#${colors.base0B}";
+      color3 = "#${colors.base0A}";
+      color4 = "#${colors.base0D}";
+      color5 = "#${colors.base0E}";
+      color6 = "#${colors.base0C}";
+      color7 = "#${colors.base05}";
+      color8 = "#${colors.base03}";
+      color9 = "#${colors.base08}";
+      color10 = "#${colors.base0B}";
+      color11 = "#${colors.base0A}";
+      color12 = "#${colors.base0D}";
+      color13 = "#${colors.base0E}";
+      color14 = "#${colors.base0C}";
+      color15 = "#${colors.base07}";
+      color16 = "#${colors.base09}";
+      color17 = "#${colors.base0F}";
+      color18 = "#${colors.base01}";
+      color19 = "#${colors.base02}";
+      color20 = "#${colors.base04}";
+      color21 = "#${colors.base06}";
 
       #cursor = "#cccccc";
       #cursor_text_color = "#111111";
@@ -145,6 +186,9 @@ in
       scrollback_lines = 10000;
       enable_audio_bell = true;
       update_check_interval = 0;
+
+      disable_ligatures = "always";  # Avoid ambiguous glyphs
+      adjust_line_height = "90%";     # Compensate for small fonts
     };
 
     #environment = {
--- home-manager/user-najib/default.nix
+++ home-manager/user-najib/default.nix
@@ -179,7 +179,7 @@ in
     xpra
     bootiso
     virt-manager
-    kitty
+    #kitty
     #jfbview
     #qmmp
     ed  # an implemintation of the standard unix editor
