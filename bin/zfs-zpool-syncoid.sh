1ee6bdef23c51886b9c35d5dc6768af8847d01df
Najib Ibrahim <mnajib@gmail.com>
2025-05-26 12:29:23 +0800

    zfs dataset renamed from najibzfspool to Garden in customdesktop

--- bin/zfs-zpool-syncoid.sh
+++ bin/zfs-zpool-syncoid.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-sudo syncoid root@customdesktop.localdomain:najibzfspool1/home MyTank/backups/offsite/najibzfspool1
+#sudo syncoid root@customdesktop.localdomain:najibzfspool1/home MyTank/backups/offsite/najibzfspool1
+sudo syncoid root@customdesktop.localdomain:Garden/home MyTank/backups/offsite/Garden-home
