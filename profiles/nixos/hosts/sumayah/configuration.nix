6ecd4585817b16c0dc03be763f7c01a6432d1813
Najib Ibrahim <mnajib@gmail.com>
2025-07-31 05:59:24 +0800

    copy /etc/nixos/* from sumayah

---
+++ profiles/nixos/hosts/sumayah/configuration.nix
@@ -0,0 +1,201 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+      ./turn-off-rgb.nix
+    ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "sumayah"; #"nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "Asia/Kuala_Lumpur";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "ms_MY.UTF-8";
+    LC_IDENTIFICATION = "ms_MY.UTF-8";
+    LC_MEASUREMENT = "ms_MY.UTF-8";
+    LC_MONETARY = "ms_MY.UTF-8";
+    LC_NAME = "ms_MY.UTF-8";
+    LC_NUMERIC = "ms_MY.UTF-8";
+    LC_PAPER = "ms_MY.UTF-8";
+    LC_TELEPHONE = "ms_MY.UTF-8";
+    LC_TIME = "ms_MY.UTF-8";
+  };
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+  # Enable the GNOME Desktop Environment.
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+
+  services.flatpak.enable = true;
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "dvorak";
+  };
+
+  # Configure console keymap
+  console.keyMap = "dvorak";
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound with pipewire.
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  services.hardware.openrgb.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.a = {
+    isNormalUser = true;
+    description = "a";
+    extraGroups = [ "networkmanager" "wheel" "kvm" "adbusers" ];
+    packages = with pkgs; [
+    #  thunderbird
+    ];
+  };
+
+  users.users.najib = {
+    isNormalUser = true;
+    description = "najib";
+    extraGroups = [ "networkmanager" "wheel" "kvm" "adbusers" ];
+    packages = with pkgs; [
+    #  thunderbird
+    ];
+  };
+
+  nixpkgs.config.android_sdk.accept_license = true;
+
+  programs.adb.enable = true;  
+
+  # Install firefox.
+  programs.firefox.enable = true;
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+  programs.steam.enable = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    wget
+    tmux
+    neovim
+    mosh
+    pulseaudioFull
+    pulsemixer
+    mission-center
+    ranger
+    gnomeExtensions.vitals
+    unzip
+    blender-hip
+    unrar
+    p7zip
+    tldr
+    pigz
+    android-studio # android-studio-full
+    kmonad
+    discord
+    htop
+    flatpak
+    gimp
+    freecad
+    steam
+
+    qbittorrent
+    bottles
+    zeroadPackages.zeroad-unwrapped
+    minetest
+    firefox
+    brave
+    varia
+    git
+
+    input-leap
+    #barrier
+
+    libreoffice
+
+    telegram-desktop
+    whatsie # whatsapp client
+    whatsapp-for-linux # whatsapp client
+    kchat # kde app
+    hexchat # IRC client
+    #fluffychat # matrix client. Marked as insecure package.
+    #deltachat-desktop # email-base IM
+    simplex-chat-desktop
+    #keet # (unfree) P2P chat
+    mumble
+    
+    openrgb-with-all-plugins
+  ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  networking.firewall.allowedTCPPorts = [ 16216 ];
+  networking.firewall.allowedUDPPorts = [ 16216 ];
+  # Or disable the firewall altogether.
+  networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "24.11"; # Did you read the comment?
+
+}
