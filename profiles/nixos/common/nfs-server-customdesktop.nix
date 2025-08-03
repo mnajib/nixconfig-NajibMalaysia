2a14c7162997133b21c8b719307be1e99dc25b35
NajibMalaysia <mnajib@gmail.com>
2025-08-03 09:25:52 +0800

    nfs-server-customdesktop.nix: zfs: use Garden (the new pool) instead of (old) najibzfspool1

--- profiles/nixos/common/nfs-server-customdesktop.nix
+++ profiles/nixos/common/nfs-server-customdesktop.nix
@@ -7,51 +7,9 @@
 # systemctl list-dependencies --before sysstat-collect.timer
 
 {
-  # Mount gluster volume
-  #fileSystems."/mnt/gluster" = {
-  #  device = "mahirah:/sekolah";
-  #  fsType = "fuse.glusterfs";
-  #  options = [
-  #    "x-systemd.automount"
-  #    "noauto"
-  #  ];
-  #};
-
-  # Kumpul directory untuk NFS service di /export
-  #fileSystems."/export/sekolahgluster" = {
-  #  device = "/mnt/gluster";
-  #  options = [
-  #    "bind"
-  #    #"nfsvers=3"
-  #    "x-systemd.automount"
-  #    "noauto"
-  #    #"x-systemd.idle-timeout=600" # # disconnects after 10 minutes (i.e. 600 seconds)
-  #  ];
-  #};
-
-  # Kumpul directory untuk NFS service di /export
-  #fileSystems."/export/sekolahdocdir" = {
-  #  device = "/home/najib/Documents/2021-01-21 Sekolah 2021";
-  #  options = [
-  #    "bind"
-  #    #"nfsvers=3"
-  #    "x-systemd.automount"
-  #    "noauto"
-  #    #"x-systemd.idle-timeout=600" # # disconnects after 10 minutes (i.e. 600 seconds)
-  #  ];
-  #};
-
-  #fileSystems."/export/documents" = {
-  #  device = "/home/najib/Documents";
-  #  options = [
-  #    "bind"
-  #    "x-systemd.automount"
-  #    "noauto"
-  #  ];
-  #};
 
   fileSystems."/mnt/data" = {
-    device = "najibzfspool1/home";
+    device = "Garden/home"; #"najibzfspool1/home";
     fsType = "zfs";
     options = [
       "x-systemd.before=export-nfsshare2.automount" #???
@@ -165,13 +123,6 @@
     lockdPort = 4001;
     mountdPort = 4002;
     statdPort = 4000;
-
-    #extraNfsdConfig = ''
-    #'';
-
-    #/export/sekolahgluster     *(rw,nohide,insecure,no_subtree_check,fsid=1)
-    #/export/sekolahdocdir      *(rw,nohide,insecure,no_subtree_check)
-    #/export/documents          *(rw,nohide,insecure,no_subtree_check)
 
     exports = ''
       /export                   *(rw,fsid=0,no_subtree_check,fsid=0)
