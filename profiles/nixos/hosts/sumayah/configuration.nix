f34d181b35c7a75919693fead25d8f59cfccffa2
Najib Ibrahim <mnajib@gmail.com>
2025-08-12 01:28:39 +0800

    sumayah: change from gdm to lightdm

--- profiles/nixos/hosts/sumayah/configuration.nix
+++ profiles/nixos/hosts/sumayah/configuration.nix
@@ -87,8 +87,10 @@ in
   services.xserver.enable = true;
 
   # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
+  #services.xserver.displayManager.gdm.enable = true;
+  services.xserver.displayManager.lightdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
+  services.displayManager.defaultSession = "none+xmonad";
 
   services.flatpak.enable = true;
 
