5055f1591ee9739311d7fc2d0a556a8e07fa7b0a
Najib Ibrahim <mnajib@gmail.com>
2025-08-17 20:22:47 +0800

    nixos: inputs.nixpkgs; nyxora: disko drive3

--- flake.nix
+++ flake.nix
@@ -11,14 +11,18 @@
   };
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
     nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-25.05";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixpkgs-master.url = "github:nixos/nixpkgs/master";
+    #nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    nixpkgs.follows = "nixpkgs-stable"; # Make 'nixpkgs' point to nixpkgs-stable
 
     flake-parts.url = "github:hercules-ci/flake-parts";
 
-    disko.url = "github:nix-community/disko";
+    disko = {
+      url = "github:nix-community/disko";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
     lix-module = {
       url = "https://git.lix.systems/lix-project/nixos-module/archive/2.91.1-1.tar.gz";
--- profiles/nixos/hosts/nyxora/disko/drive3.nix
+++ profiles/nixos/hosts/nyxora/disko/drive3.nix
@@ -1,4 +1,21 @@
 # disko-config.nix
+
+#
+# To read more about disko:
+#   https://github.com/nix-community/disko/blob/master/docs/quickstart.md
+#
+# To download the example config:
+#   curl https://raw.githubusercontent.com/nix-community/disko/master/example/hybrid.nix -o /tmp/disko-config.nix
+#
+# To apply the config to the drive(s):
+#   sudo nix \
+#     --experimental-features "nix-command flakes" \
+#     run github:nix-community/disko -- \
+#     --mode disko /tmp/disko-config.nix
+#
+# To generate initial NixOS configuration
+# but without config to mount the filesystems, because the filesystems is already configured (mount) by disko's nixosModule.
+#
 {
   #disko.devices = {
 
@@ -11,11 +28,13 @@
       content = {
         type = "gpt";
         partitions = {
+
           boot_bios = {
             size = "2M"; #"1M";
             #type = "bios_grub";
             type = "EF02"; # use MBR
           };
+
           boot_efi = {
             size = "1G";
             type = "EF00";
@@ -23,8 +42,10 @@
               type = "filesystem";
               format = "vfat";
               mountpoint = "/boot1003"; # temp mountpoint. named to avoid conflict with existing /boot and /boot2, for now
+              mountOptions = [ "umask=0077" ];
             };
           };
+
           swap = {
             size = "8G";
             type = "8200";
@@ -33,14 +54,16 @@
               resumeDevice = true;
             };
           };
+
           zfs = {
             size = "100%";
             type = "bf01"; # Solaris /usr & Apple ZFS
             # Do not format here, ZFS will handle this
           };
-        };
-      };
-    };
+
+        }; # End partitions = { ... };
+      }; # End content = { ... };
+    }; # End disk.GCNL = { ... };
     #--------------------------------------------------------------------------
 
   #}; # End disko.devices = { ... };
