6cfb2d39d036b6c6c3e39648aab8c0b2a77c7d09
Najib Ibrahim <mnajib@gmail.com>
2025-09-29 16:20:25 +0800

    home-manager: games.nix: colobot

---
+++ profiles/home-manager/common/games.nix
@@ -0,0 +1,19 @@
+{
+  inputs,
+  outputs,
+  #lib,
+  config,
+  pkgs,
+  ...
+}:
+#let
+#in
+{
+  #imports = [
+  #];
+
+  home.packages = with pkgs; [
+    colobot # a real-time strategy game, where you can program your bots
+  ];
+
+}
