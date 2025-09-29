fa2bce421d4f369795f547a6d23b8c89238cb7e1
Najib Ibrahim <mnajib@gmail.com>
2025-09-29 13:06:49 +0800

    add some desktop apps

--- profiles/home-manager/common/desktop-apps.nix
+++ profiles/home-manager/common/desktop-apps.nix
@@ -19,6 +19,21 @@
     # Web browser
     #firefox
     librewolf
+
+    # Create a pdf
+    rst2pdf
+    python313Packages.reportlab
+    typst # markup-based typesetting system that is powerful and easy to learn
+    typstyle # Format your typst source code
+    python313Packages.weasyprint # Converts web documents to PDF
+
+    # Create a presentation
+    typstPackages.polylux # a package for the typesetting system Typst to create presentation slides
+    typstwriter # GUI tool / text editor to help writing typs document
+    polylux2pdfpc # convert polylux to pdfpc
+
+    # Do a presentatation
+    pdfpc # presentation from PDF file
   ];
 
 }
