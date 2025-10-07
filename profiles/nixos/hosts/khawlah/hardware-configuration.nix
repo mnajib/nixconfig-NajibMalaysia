bdbe93e33380d1de4561e8fa43376fe6267cae0f
Najib Ibrahim <mnajib@gmail.com>
2025-10-07 08:15:53 +0800

    for fresh install khawlah using nixos-anywhere

--- profiles/nixos/hosts/khawlah/hardware-configuration.nix
+++ profiles/nixos/hosts/khawlah/hardware-configuration.nix
@@ -8,7 +8,7 @@
     [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "xhci_pci" "ehci_pci" "ahci" "usb_storage" "sd_mod" "sdhci_pci" ];
+  boot.initrd.availableKernelModules = [ "xhci_pci" "ehci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" "sdhci_pci" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
