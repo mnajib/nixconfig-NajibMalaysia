54f744ade65f6c6ac3c9550ee7b6bb2b5f136c8e
Najib Ibrahim <mnajib@gmail.com>
2025-11-01 19:47:05 +0800

    hm: git: git alias for simplified view of branches

--- profiles/home-manager/common/git.nix
+++ profiles/home-manager/common/git.nix
@@ -41,13 +41,15 @@
           #hist = "log --pretty=format:'%C(yellow)%h%Cred%d%Creset - %C(cyan)%an %Creset: %s %Cgreen(%cr)' --graph --date=short --all";           #
           hist = "log --pretty=format:'%C(yellow)%h%Cred%d%Creset - %C(cyan)%an %Creset: %s %Cgreen(%cd)' --graph --date=short --all";            #
           #hist = "log --pretty=format:'%C(yellow)%h%Cred%d%Creset - %C(cyan)%an %Creset: %s %Cgreen(%cd)' --graph --date=relative --all";        #
-          histp = "log --pretty=format:'%C(yellow)%h%Cred%d%Creset - %C(cyan)%an %Creset: %s %Cgreen(%cd)' --graph --date=short --all -p";        #
-          hist2 = "log --graph --oneline --simplify-by-decoration --all";                   #
-          hist3 = "log --graph --oneline --decorate --all";                                 #
-          hist4 = "log --stat --graph --pretty=format:'%h - %an: %s (%cd)' --all";          # '--pretty=format:' show custom commit info.
-          hist5 = "log --graph --pretty=format:'%h - %an: %s (%cd)' --numstat --all";       # '--numstat' show files changed with numerac stats, easier to compute percentages.
-          hist6 = "log --graph --pretty=format:'%C(yellow)%h%Creset - %C(cyan)%an%Creset: %s %Cgreen(%cd)' --stat --all";         # '--stat' show files changed and lines added/deleted (summary).
-          hist7 = "log --graph --pretty=format:'%C(yellow)%h%Creset - %C(cyan)%an%Creset: %s %Cgreen(%cd)' -p --all";             # '-p' show full patch/diff per commit.
+          histp   = "log --pretty=format:'%C(yellow)%h%Cred%d%Creset - %C(cyan)%an %Creset: %s %Cgreen(%cd)' --graph --date=short --all -p";        #
+          hist2   = "log --graph --oneline --simplify-by-decoration --all";                   #
+          #hist22  = "log --graph --pretty=format:'%C(auto)%h %C(blue)%d %C(reset)%s' --simplify-by-decoration --all --color=always | grep --color=always -v 'tag:'";
+          hist22 = "!sh -c 'git log --graph --pretty=format:\"%C(auto)%h %C(blue)%d %C(reset)%s\" --simplify-by-decoration --all --color=always | grep --color=always -v \"tag:\"'";
+          hist3   = "log --graph --oneline --decorate --all";                                 #
+          hist4   = "log --stat --graph --pretty=format:'%h - %an: %s (%cd)' --all";          # '--pretty=format:' show custom commit info.
+          hist5   = "log --graph --pretty=format:'%h - %an: %s (%cd)' --numstat --all";       # '--numstat' show files changed with numerac stats, easier to compute percentages.
+          hist6   = "log --graph --pretty=format:'%C(yellow)%h%Creset - %C(cyan)%an%Creset: %s %Cgreen(%cd)' --stat --all";         # '--stat' show files changed and lines added/deleted (summary).
+          hist7   = "log --graph --pretty=format:'%C(yellow)%h%Creset - %C(cyan)%an%Creset: %s %Cgreen(%cd)' -p --all";             # '-p' show full patch/diff per commit.
 
           type = "cat-file -t";
           dump = "cat-file -p";
