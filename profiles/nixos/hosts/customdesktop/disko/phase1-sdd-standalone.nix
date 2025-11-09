ab5b161c16831d7620ca37e86f63887b5ebdbf4e
Najib Ibrahim <mnajib@gmail.com>
2025-11-09 15:04:36 +0800

    customdesktop: disko CLI not mount and activate swap

--- profiles/nixos/hosts/customdesktop/disko/phase1-sdd-standalone.nix
+++ profiles/nixos/hosts/customdesktop/disko/phase1-sdd-standalone.nix
@@ -2,6 +2,9 @@
 # Run it with:
 #   sudo nix run github:nix-community/disko -- --mode disko ./phase1-sdd-standalone.nix
 #
+# WARNING!
+#   This will ...
+#
 
 { lib }: {
   disko.devices = import ./phase1-sdd-definition.nix { inherit lib; };
