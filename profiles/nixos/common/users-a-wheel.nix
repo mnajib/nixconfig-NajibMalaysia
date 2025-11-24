68bf310f497e3bed8917f135ed4cb0ec726908f6
Najib Ibrahim <mnajib@gmail.com>
2025-11-24 20:35:16 +0800

    nixos: initial temporary user password

--- profiles/nixos/common/users-a-wheel.nix
+++ profiles/nixos/common/users-a-wheel.nix
@@ -3,26 +3,25 @@
   ...
 }:
 {
-    users.users.a = {
-        uid = 1000;
-        #initialPassword = "password";
-
-        description = "a";
-        isNormalUser = true;
-        createHome = true;
-        home = "/home/a";
-        extraGroups = [
-		"networkmanager" "audio" "video" "cdrom" "scanner" "lp" "systemd-journal"
-		"istana46" "users"
-		"dialout"
-		"wheel"
-	];
-	packages = with pkgs; [
-		firefox
-		tmux
-		neovim
-		htop
-		git
-	];
-    };
+  users.users.a = {
+    uid = 1000;
+    initialPassword = "password";
+    description = "a";
+    isNormalUser = true;
+    createHome = true;
+    home = "/home/a";
+    extraGroups = [
+      "networkmanager" "audio" "video" "cdrom" "scanner" "lp" "systemd-journal"
+      "istana46" "users"
+      "dialout"
+      "wheel"
+    ];
+    packages = with pkgs; [
+      firefox
+      tmux
+      neovim
+      htop
+      git
+    ];
+  };
 }
