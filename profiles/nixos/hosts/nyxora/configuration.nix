6c4229947cf4ea0a5f457238f05a26ad229842e9
Najib Ibrahim <mnajib@gmail.com>
2025-11-27 11:15:21 +0800

    nyxora: postgresql, docker, airbyte

--- profiles/nixos/hosts/nyxora/configuration.nix
+++ profiles/nixos/hosts/nyxora/configuration.nix
@@ -126,6 +126,7 @@ in
     #(./. + "/${commonDir}/forgejo-sqlite.nix")
     #(fromCommon "forgejo-sqlite.nix")
     (fromCommon "forgejo-sqlite-nyxora.nix")
+    (fromCommon "/services/postgresql-nyxora.nix")
 
     #./hosts2.nix
 
