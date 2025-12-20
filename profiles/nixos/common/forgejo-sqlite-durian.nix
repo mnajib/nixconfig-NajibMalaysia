9a5d28751585d1e0005560e91d56db05ad7b1e71
Muhammad Najib <mnajib@gmail.com>
2025-12-20 08:16:12 +0800

    durian: forjego, najib

---
+++ profiles/nixos/common/forgejo-sqlite-durian.nix
@@ -0,0 +1,82 @@
+{ lib, pkgs, config, ... }:
+let
+  forgejo_port = 3000;
+  forgejo_user = "forgejo"; #"git";
+  forgejo_group = "forgejo"; #"git";
+  forgejo_uid = 990; #993
+  forgejo_gid = 987; #990
+  #forgejo_stateDir = "/var/lib/forgejo";
+  #forgejo_stateDir = "/MyTank/services/forgejo";
+  forgejo_stateDir = "/mnt/data/forgejo";
+in
+{
+  users.users.${forgejo_user} = {
+    uid = forgejo_uid;
+    isSystemUser = true;
+    group = forgejo_group;
+    home = forgejo_stateDir;
+    createHome = true;
+  };
+  users.extraGroups.${forgejo_group}.gid = forgejo_gid;
+  users.groups.${forgejo_group} = {}; # Brings other users inside this group
+
+  services.forgejo = {
+    enable = true;
+
+    user = forgejo_user;
+    group = forgejo_group;
+
+    stateDir = forgejo_stateDir;
+
+    database.type = "sqlite3";
+
+    settings = {
+      server = {
+        DOMAIN = "${config.networking.hostName}";
+        # You need to specify this to remove the port from URLs in the web UI.
+        #ROOT_URL = "https://${srv.DOMAIN}/";
+        #HTTP_PORT = port_number;
+      };
+
+    }; # End services.forgejo.settings
+
+    #secrets = {
+    #  metrics = {
+    #    TOKEN = "/run/keys/forgejo-metrics-token";
+    #  };
+    #  camo = {
+    #    HMAC_KEY = "/run/keys/forgejo-camo-hmac";
+    #  };
+    #  service = {
+    #    HCAPTCHA_SECRET = "/run/keys/forgejo-hcaptcha-secret";
+    #    HCAPTCHA_SITEKEY = "/run/keys/forgejo-hcaptcha-sitekey";
+    #  };
+    #};
+
+    #mailerPasswordFile = config.age.secrets.forgejo-mailer-password.path;
+
+  }; # End services.forgejo
+
+  #age.secrets.forgejo-mailer-password = {
+  #  file = ../secrets/forgejo-mailer-password.age;
+  #  mode = "400";
+  #  owner = "forgejo";
+  #};
+
+  # Need to mount the my zfs storage first
+  systemd.services.forgejo = {
+    after = [
+      "mnt-data.automount"
+    ];
+  };
+
+  networking.firewall = {
+    allowedTCPPorts = [
+      forgejo_port
+      #3000
+    ];
+    allowedUDPPorts = [
+      #...
+    ];
+  };
+}
