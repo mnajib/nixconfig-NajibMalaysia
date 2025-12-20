952ed72b941d39533d3bda0942b8d3f544bc0396
Najib Ibrahim <mnajib@gmail.com>
2025-12-20 12:45:12 +0800

    nixos: services/bind.nix: update, +durian +reverse

--- profiles/nixos/common/services/bind.nix
+++ profiles/nixos/common/services/bind.nix
@@ -3,6 +3,66 @@
   pkgs,
   ...
 }:
+let
+  hosts = {
+    ns1             = "192.168.0.11";
+    gw              = "192.168.0.1";
+
+    customdesktop   = "192.168.0.10";
+    durian          = "192.168.0.10";
+    nyxora          = "192.168.0.11";
+    printer         = "192.168.0.22";
+
+    taufiq          = "192.168.0.12";
+    sumayah         = "192.168.0.13";
+  };
+
+  zoneFile = pkgs.writeText "zone-localdomain" ''
+    $ORIGIN localdomain.
+    $TTL    1h
+
+    @               IN SOA ns1.localdomain. hostmaster.localdomain. (
+                        1        ; Serial
+                        3h       ; Refresh
+                        1h       ; Retry
+                        1w       ; Expire
+                        1h       ; Negative Cache TTL
+                    )
+
+                    IN NS ns1.localdomain.
+
+    ${pkgs.lib.concatStringsSep "\n" (pkgs.lib.mapAttrsToList (name: ip: "${name} IN A ${ip}") hosts)}
+  '';
+
+  reverseZone = "0.168.192.in-addr.arpa";
+
+  reverseFile = pkgs.writeText "zone-reverse" ''
+    $ORIGIN ${reverseZone}.
+    $TTL    1h
+
+    @               IN SOA ns1.localdomain. hostmaster.localdomain. (
+                        1        ; Serial
+                        3h       ; Refresh
+                        1h       ; Retry
+                        1w       ; Expire
+                        1h       ; Negative Cache TTL
+                    )
+
+                    IN NS ns1.localdomain.
+
+    ${pkgs.lib.concatStringsSep "\n" (
+      pkgs.lib.unique (
+        pkgs.lib.mapAttrsToList (name: ip:
+          let
+            octets = pkgs.lib.splitString "." ip;
+            last = builtins.elemAt octets 3;
+          in
+            "${last} IN PTR ${name}.localdomain."
+        ) hosts
+      )
+    )}
+  '';
+in
 {
   services.bind = {
     enable = true;
@@ -31,31 +91,43 @@
 
     # Authoritative zone for localdomain
     zones = {
+
+      #"localdomain" = {
+      #  master = true;
+      #  file = pkgs.writeText "zone-localdomain" ''
+      #    $ORIGIN localdomain.
+      #    $TTL    1h
+      #
+      #    @               IN SOA ns1.localdomain. hostmaster.localdomain. (
+      #                        1        ; Serial
+      #                        3h       ; Refresh
+      #                        1h       ; Retry
+      #                        1w       ; Expire
+      #                        1h       ; Negative Cache TTL
+      #                    )
+      #
+      #                    IN NS ns1.localdomain.
+      #
+      #    ns1             IN A 192.168.0.11
+      #    gw              IN A 192.168.0.1
+      #    customdesktop   IN A 192.168.0.10
+      #    nyxora          IN A 192.168.0.11
+      #    printer         IN A 192.168.0.22
+      #    taufiq          IN A 192.168.0.12
+      #    sumayah         IN A 192.168.0.13
+      #  '';
+      #}; # End zones."localdomain" = { ... };
+
       "localdomain" = {
         master = true;
-        file = pkgs.writeText "zone-localdomain" ''
-          $ORIGIN localdomain.
-          $TTL    1h
+        file = zoneFile;
+      };
 
-          @               IN SOA ns1.localdomain. hostmaster.localdomain. (
-                              1        ; Serial
-                              3h       ; Refresh
-                              1h       ; Retry
-                              1w       ; Expire
-                              1h       ; Negative Cache TTL
-                          )
+      "${reverseZone}" = {
+        master = true;
+        file = reverseFile;
+      };
 
-                          IN NS ns1.localdomain.
-
-          ns1             IN A 192.168.0.11
-          gw              IN A 192.168.0.1
-          customdesktop   IN A 192.168.0.10
-          nyxora          IN A 192.168.0.11
-          printer         IN A 192.168.0.22
-          taufiq          IN A 192.168.0.12
-          sumayah         IN A 192.168.0.13
-        '';
-      }; # End zones."localdomain" = { ... };
     };
 
     # Extra options for recursion and forwarding
