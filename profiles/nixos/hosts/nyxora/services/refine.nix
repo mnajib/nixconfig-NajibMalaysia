10c284738d981504908bfc06c2d97eba377963d9
Najib Ibrahim <mnajib@gmail.com>
2026-01-06 21:01:33 +0800

    nyxora: refine: sijilberhenti app accessible with nginx as f~rontend

--- profiles/nixos/hosts/nyxora/services/refine.nix
+++ profiles/nixos/hosts/nyxora/services/refine.nix
@@ -1,4 +1,9 @@
-{ config, pkgs, ... }:
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
 
 let
   #refine_stateDir = "/MyTank/services/refine"; # Need 'mount /MyTank/services' and 'mkdir /MyTank/services/refine'
@@ -19,48 +24,13 @@ let
   refine_group = "refine";
   refine_uid = 400;
   refine_gid = 400;
+  refine_root = "/MyTank/services/refine";
 
   # A helper function to create a Refine service
-  mkRefineService = { name, port, domain }: {
-    systemd.services."refine-${name}" = {
-      description = "Refine App: ${name}";
+  #mkRefineApp = { name, dev_port, domain }: {
+  mkRefineApp = { name, domain }: {
+    # Phase-1: only nginx reverse proxy, app build
 
-      # Start this service after mount the my zfs storage first
-      after = [
-        "MyTank-services.mount"
-        "zfs-mount.service"
-        "network.target"
-      ];
-
-      wantedBy = [
-        #"multi-user.target"
-      ];
-
-      # Explicit requires: won’t even try to start if the mount isn’t there.
-      requires = [
-        "MyTank-services.mount"
-      ];
-
-      # This makes 'node' and 'npm' available ONLY to this service
-      #path = [ pkgs.nodejs_24 ];
-
-      serviceConfig = {
-        User = "refine"; # They can share a system user or have unique ones
-        Group = "refine";
-        StateDirectory = "refine/${name}"; # XXX
-        #WorkingDirectory = "/var/lib/refine/${name}";
-        WorkingDirectory = "/MyTank/services/refine/${name}";
-        #Environment = "PORT=${toString port}"; # Each app needs a unique port
-        #ExecStart = "${pkgs.nodejs}/bin/npm start";
-        ExecStart = "${pkgs.nodejs_24}/bin/npm start -- --port ${toString port}";
-
-        #Restart = "always";
-        Restart = "on-failure";
-        RestartSec = 5;
-      };
-    };
-
-    #services.nginx.virtualHosts."app.localdomain" = {
     services.nginx.virtualHosts."${domain}" = {
       #sslCertificate     = "/etc/nixos/secrets/tls/app.home.crt";
       #sslCertificateKey  = "/etc/nixos/secrets/tls/app.home.key";
@@ -72,15 +42,26 @@ let
       addSSL = false;                   # 'true' to configures Nginx listen on both Port 80 (HTTP) and Port 443 (HTTPS).
       onlySSL = false;                  # 'true' to configures Nginx to listen only on Port 443. HTTP requests will simply fail.
 
+      root = "${refine_root}/${name}/dist";
+
       locations."/" = {
         #proxyPass = "http://127.0.0.1:5173"; # Default Vite port
-        proxyPass = "http://127.0.0.1:${toString port}";
+        #proxyPass = "http://127.0.0.1:${toString port}";
         #proxyPass = "http://unix:/run/postgrest/postgrest.sock";
-        proxyWebsockets = true; # Necessary for Hot Module Replacement (HMR)
+        #proxyWebsockets = true; # Necessary for Hot Module Replacement (HMR)
+        tryFiles = "$uri $uri/ /index.html";
       };
     };
-
   };
+
+  # -----------------------------
+  # List of Refine apps
+  # -----------------------------
+  refineApps = [
+    { name = "sijilberhenti"; domain = "sijilberhenti.localdomain"; }
+    #{ name = "app2";          domain = "app2.localdomain"; }
+    #{ name = "app3";          domain = "app3.localdomain"; }
+  ];
 in
 {
   fileSystems."/MyTank/services" = {
@@ -95,14 +76,26 @@ in
     #home = "/var/lib/refine";
     home = "/MyTank/services/refine";
     createHome = true;
+    shell = pkgs.bash; # ???
   };
 
   users.groups."${refine_group}" = {
     gid = refine_gid;
     members = [
       "${refine_user}"
+      "nginx"
     ];
   };
+
+  # NOTE:
+  # Fix directory permissions (need only run one-time)
+  #sudo chmod 755 /MyTank
+  #sudo chmod 755 /MyTank/services
+  #sudo chmod 755 /MyTank/services/refine
+  #sudo chmod 755 /MyTank/services/refine/sijilberhenti
+  #sudo chmod 755 /MyTank/services/refine/sijilberhenti/dist
+  # Verify
+  #namei -l /MyTank/services/refine/sijilberhenti/dist/index.html
 
   # Add nginx user to the postgrest group so it can read the socket
   users.users.nginx.extraGroups = [
@@ -111,17 +104,27 @@ in
 
   services.nginx.enable = true;
 
+  networking.firewall.enable = lib.mkDefault true;
+  networking.firewall.allowedTCPPorts = [ 80 443 ]; # HTTP + HTTPS
+
   environment.systemPackages = with pkgs; [
     nodejs_24
+    yarn
   ];
 
   # Define your apps here in one line each!
   # This executes the logic for app1, app2, etc.
-  imports = [
-    #(mkRefineService { name = "crm";       port = 3000; domain = "crm.example.com"; })
-    #(mkRefineService { name = "inventory"; port = 3001; domain = "stock.example.com"; })
-    #(mkRefineService { name = "billing";   port = 3002; domain = "invoice.example.com"; })
-    (mkRefineService { name = "app";       port = 3003; domain = "app.localdomain"; })
-  ];
+  #imports = [
+  #  #(mkRefineService { name = "crm";       port = 3000; domain = "crm.example.com"; })
+  #  #(mkRefineService { name = "inventory"; port = 3001; domain = "stock.example.com"; })
+  #  #(mkRefineService { name = "billing";   port = 3002; domain = "invoice.example.com"; })
+  #  #(mkRefineService { name = "app";       port = 3003; domain = "app.localdomain"; })
+  #  (mkRefineService { name = "sijilberhenti";       port = 3003; domain = "sijilberhenti.localdomain"; })
+  #];
+  #
+  # -----------------------------
+  # Import each Refine app nginx configuration
+  # -----------------------------
+  imports = map (app: mkRefineApp app) refineApps;
 }
 
