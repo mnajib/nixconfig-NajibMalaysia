286cfa08ed3da6a7616570b4dd8a02dda85e1e26
Najib Ibrahim <mnajib@gmail.com>
2026-05-26 13:27:55 +0800

    nixos: kmscon: use dvorak

--- profiles/nixos/common/kmscon.nix
+++ profiles/nixos/common/kmscon.nix
@@ -1,4 +1,9 @@
-{ pkgs, config, ... }:
+{
+  pkgs,
+  config,
+  #lib,
+  ...
+}:
 {
   imports = [
     #./steam.nix
@@ -16,8 +21,24 @@
       { name = "Source Code Pro"; package = pkgs.source-code-pro; }
     ];
     extraOptions = "--term xterm-256color";
-    extraConfig = "font-size=12";
+    #extraConfig = "font-size=12";
     hwRender = true;
-    useXkbConfig = true;
+
+    extraConfig = ''
+      font-name=Source Code Pro
+      font-size=12
+      xkb-layout=us
+      xkb-variant=dvorak
+    '';
+
   };
+
+  console = {
+    enable = true;
+    # Inherits from the XKB parameters if useXkbConfig is supported by vconsole as well
+    #useXkbConfig = true;
+    # Fallback to standard kernel keymap for early boot initrd stages
+    keyMap = "dvorak";
+  };
+
 }
