ddfc727a38decc2a7a2303a0827e1e11180ce824
Najib Ibrahim <mnajib@gmail.com>
2026-06-12 11:23:10 +0800

    bawang: add some packages: mosh dig deskflow telegram

--- profiles/nixos/hosts/bawang/configuration.nix
+++ profiles/nixos/hosts/bawang/configuration.nix
@@ -145,6 +145,7 @@
     git
     tmux
     ripgrep-all
+    dig
 
     neovim
     vim
@@ -180,6 +181,8 @@
     calibre
 
     xournalpp
+    deskflow
+    telegram-desktop
 
     nnn ranger
 
@@ -199,6 +202,7 @@
 
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
+  programs.mosh.enable = true;
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
