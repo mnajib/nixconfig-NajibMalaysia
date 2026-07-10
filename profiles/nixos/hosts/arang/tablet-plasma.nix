3b0acca252ba52d7a551e4afebea93cae4897d03
Najib Ibrahim <mnajib@gmail.com>
2026-07-10 15:05:16 +0800

    arang: tablet-plasma.nix

--- profiles/nixos/hosts/arang/tablet-plasma.nix
+++ profiles/nixos/hosts/arang/tablet-plasma.nix
@@ -29,9 +29,10 @@
   */
 
 
-  programs.dconf.enable = true;
+  #programs.dconf.enable = true;
   # Note: Defining dconf settings at the system level requires configuring a user profile database.
 
+  /*
   programs.dconf.profiles.user.databases = [
     {
       settings = {
@@ -47,22 +48,28 @@
       };
     }
   ];
+  */
+  # Explicitly clean out the system-level dconf profile leftover from GNOME
+  programs.dconf.profiles.user.databases = [];
 
-  i18n.inputMethod = {
-    enable = true;
-    type = "ibus";
-  };
+  #i18n.inputMethod = {
+  #  enable = true;
+  #  type = "ibus";
+  #};
 
   # Enables hardware (orientation) sensor sensing for automatic screen rotation
   hardware.sensor.iio.enable = true;
 
+  # Enable the Maliit Input Method Framework
+  #programs.maliit.enable = true;
+
   # This is the cleanest approach. Nix looks at gnomeExtensions first. If you
   # list improved-osk, it finds it there. If you were to list git, Nix wouldn't
   # find it in gnomeExtensions, so it would fall back to checking pkgs.
-  environment.systemPackages = with pkgs; with gnomeExtensions; [
+  #environment.systemPackages = with pkgs; with gnomeExtensions; [
   #
   # The inherit Approach (Alternative)
-  #environment.systemPackages = with pkgs; [
+  environment.systemPackages = with pkgs; [
 
     # From pkgs
     #git
@@ -74,13 +81,17 @@
     # From pkgs.gnomeExtensions
     #improved-onscreen-keyboard
     #im-panel-integrated-with-osk
-    touchup
-    kmonad-toggle
-    keyboard-toggle
+    #touchup
+    #kmonad-toggle
+    #keyboard-toggle
 
-    awesome-tiles
+    maliit-keyboard
+    maliit-framework
+    kdePackages.plasma-keyboard
 
-    al-hijri-date
+    #awesome-tiles
+
+    #al-hijri-date
     #better-desktop-zoom
     #battery-time-with-percentage
 
