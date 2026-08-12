e6e5ec5267cc9d51371bfd3341279cd412f8a20f
Najib Ibrahim <mnajib@gmail.com>
2026-08-12 22:50:30 +0800

    HM: niri: waybar: now use data from waktusolat-NajibMalaysia

--- profiles/home-manager/common/niri-desktop/waybar/scripts/waktusolat.sh
+++ profiles/home-manager/common/niri-desktop/waybar/scripts/waktusolat.sh
@@ -18,7 +18,8 @@
 
 #!/usr/bin/env bash
 
-PRAYER_REMINDER_FILE="/tmp/${USER}-prayer_reminder_file"
+#PRAYER_REMINDER_FILE="/tmp/${USER}-prayer_reminder_file"
+PRAYER_REMINDER_FILE="/run/waktusolat/reminder.json"
 
 if [ -f "$PRAYER_REMINDER_FILE" ]; then
     RAW_CONTENT=$(cat "$PRAYER_REMINDER_FILE")
@@ -26,15 +27,17 @@ else
     RAW_CONTENT="Waiting for prayer data..."
 fi
 
-# Convert xmobar tags to valid Pango markup safely
-# Using [^,>]+ ensures we strictly match hex codes and nothing else
-PANGO_CONTENT=$(echo "$RAW_CONTENT" | sed -E \
-    -e 's/<fc=([^,>]+),([^>]+)>/<span foreground="\1" background="\2">/g' \
-    -e 's/<fc=([^,>]+)>/<span foreground="\1">/g' \
-    -e 's/<\/fc>/<\/span>/g')
+echo "${RAW_CONTENT}"
 
-# Escape the string for JSON safety using jq
-SAFE_TEXT=$(echo "$PANGO_CONTENT" | jq -aRs .)
+## Convert xmobar tags to valid Pango markup safely
+## Using [^,>]+ ensures we strictly match hex codes and nothing else
+#PANGO_CONTENT=$(echo "$RAW_CONTENT" | sed -E \
+#    -e 's/<fc=([^,>]+),([^>]+)>/<span foreground="\1" background="\2">/g' \
+#    -e 's/<fc=([^,>]+)>/<span foreground="\1">/g' \
+#    -e 's/<\/fc>/<\/span>/g')
+
+## Escape the string for JSON safety using jq
+#SAFE_TEXT=$(echo "$PANGO_CONTENT" | jq -aRs .)
 
 # Output in Waybar JSON format
-printf '{"text": %s, "tooltip": "Prayer Times Schedule"}\n' "$SAFE_TEXT"
+#printf '{"text": %s, "tooltip": "Prayer Times Schedule"}\n' "$SAFE_TEXT"
